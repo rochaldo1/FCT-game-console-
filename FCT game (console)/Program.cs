fd10fa9498c1fbde85dbfc845b7e6cb15ede4206
rochaldo1 <xusainow.artur2018@yandex.ru>
2024-11-02 16:40:23 +0700

    Now printing rules

--- FCT game (console)/Program.cs
+++ FCT game (console)/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+using FCT_game__console_.Bot;
+using FCT_game__console_.Game;
+using FCT_game__console_.Player;
+using FCT_game__console_.Menu;
+
+namespace Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RulesPrinter.Printer();
+            Console.Clear();
+
+
+        }
+    }
+}
